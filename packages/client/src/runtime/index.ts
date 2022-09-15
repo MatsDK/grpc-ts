9c002978cc53422b6e0959b53cc28724bb127613
MatsDK <67562518+MatsDK@users.noreply.github.com>
2022-09-15 19:44:11 +0200

    add unary call handler for client

--- packages/client/src/runtime/index.ts
+++ packages/client/src/runtime/index.ts
@@ -2,15 +2,17 @@ import { credentials, GrpcObject, loadPackageDefinition } from '@grpc/grpc-js'
 import { loadSync } from '@grpc/proto-loader'
 import _ from 'lodash'
 
+interface RPC {
+    requestType: string
+    requestStream: boolean
+    responseType: string
+    responseStream: boolean
+}
+
 interface Service {
     name: string
     service: {
-        methods: Record<string, {
-            requestType: string
-            requestStream: boolean
-            responseType: string
-            responseStream: boolean
-        }>
+        methods: Record<string, RPC>
     }
 }
 
@@ -90,12 +92,32 @@ const applyRpcs = ({ name, service }: Service, client: GrpcClient) => {
         const Service = _.get(client.protoDescriptor, name.slice(1)) as any
         const serviceClientImpl = new Service(client.opts.url, credentials.createInsecure())
 
-        return [rpcName, () => {
-            console.log(rpcName, serviceClientImpl)
-        }] as const
+        let handlerFn: ResolverFn = null!
+        // if (rpc.requestStream && rpc.responseStream) handlerFn = this.bidiStreamingRpcHandler(resolver)
+        // else if (rpc.requestStream) handlerFn = this.clientStreamingRpcHandler(resolver)
+        // else if (rpc.responseStream) handlerFn = this.serverStreamingRpcHandler(resolver)
+        if (!handlerFn) handlerFn = unaryCallHandler(rpcName, serviceClientImpl)
+
+        return [rpcName, handlerFn] as const
     }).reduce((prev, [rpcName, rpc]) => {
         prev[rpcName] = rpc
 
         return prev
     }, {} as Record<string, ResolverFn>)
+}
+
+const unaryCallHandler = (rpcName: string, serviceImpl: Record<string, ResolverFn>) => {
+    return async (input: unknown) => {
+        const res = await new Promise((res, rej) => {
+            if (!serviceImpl[rpcName]) rej(`${rpcName} does not exist`)
+
+            serviceImpl[rpcName](input, (err: string, response: unknown) => {
+                if (err) rej(err)
+
+                res(response)
+            })
+        })
+
+        return res
+    }
 }
